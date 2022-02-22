06f73211d43b79700d2fb43e53957e02ad4c81a3
R <streltssov@gmail.com>
2022-02-22 23:44:28 +0500

    mobile-menu added

--- src/blocks/header/header.js
+++ src/blocks/header/header.js
@@ -1,13 +1,15 @@
 function addRegHandler() {
-  const buttonReg = document.querySelector('.js-header__entry');
-  buttonReg.addEventListener('click', (e) => {
-    if (e.target.closest('.js-header__button-reg')) {
-      localStorage.setItem('entry', 'registration');
-    }
-    if (e.target.closest('.js-header__button-login')) {
-      localStorage.setItem('entry', 'login');
-    }
-    window.location.href = 'registration.html';
+  const buttonsReg = document.querySelectorAll('.js-header__entry');
+  buttonsReg.forEach((button) => {
+    button.addEventListener('click', (e) => {
+      if (e.target.closest('.js-header__button-reg')) {
+        localStorage.setItem('entry', 'registration');
+      }
+      if (e.target.closest('.js-header__button-login')) {
+        localStorage.setItem('entry', 'login');
+      }
+      window.location.href = 'registration.html';
+    });
   });
 }
 
